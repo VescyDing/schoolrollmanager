edb7612610653dac4565b7df2c12bbb62545560e
v4archer <v4archer@outlook.com>
2018-09-07 15:44:59 +0800

    Initial commit

---
+++ src/main/java/me/onespace/schoolrollmanager/SchoolrollmanagerApplication.java
@@ -0,0 +1,12 @@
+package me.onespace.schoolrollmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolrollmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolrollmanagerApplication.class, args);
+    }
+}
